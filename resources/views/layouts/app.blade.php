74afc3e8482b9a636b4f972d6201224b1e7eda9c
Toma Loredana <tomaloredana@gmail.com>
2018-09-15 16:22:16 +0300

    adaugare rute corecte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,9 @@
       <div class="row">
         <div class="top-bar-left">
           <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
-            <li role="menuitem"><a href="./home.html">Home</a></li>
-            <li role="menuitem"><a href="./clients.html">Clients</a></li>
-            <li role="menuitem"><a href="./reservations.html">Reservations</a></li>
+            <li role="menuitem"><a href="./home">Home</a></li>
+            <li role="menuitem"><a href="./clients">Clients</a></li>
+            <li role="menuitem"><a href="./reservations">Reservations</a></li>
           </ul>
         </div>
       </div>
@@ -34,7 +34,7 @@
     <div class="row column">
       <hr>
       <ul class="menu">
-        <li class="float-right">Copyright 2017</li>
+        <li class="float-right">Copyright 2018</li>
       </ul>
     </div>
 
